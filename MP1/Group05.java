72b646725679907072ac565e9b857d3c9294c361
Chris Piamonte <chrispiamonte@Chriss-MacBook-Air.local>
2025-09-20 11:28:05 +0800

    Machine Problem 1

---
+++ MP1/Group05.java
@@ -0,0 +1,19 @@
+/*
+ * ==========================================================
+ *  Machine Problem 1 - Workspace for Group 05
+ * ==========================================================
+ *
+ * Instructions:
+ * - This file is your workspace for solving Machine Problem 1.
+ * - You will practice the Programming Core Concepts discussed in class,
+ *   such as variables, data types, operators, and basic string operations.
+ * - Do NOT create additional classes or advanced features (like arrays)
+ *   unless instructed.
+ * ==========================================================
+ */
+
+public class Group05 {
+  public static void main(String[] args) {
+
+  }
+}
