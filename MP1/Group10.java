ac61c50f2c026db7499cb7545edc74839f0992de
Izeah Arquillano <izeaharquillano@gmail.com>
2025-09-20 22:03:59 +0800

    made first working prototype

--- MP1/Group10.java
+++ MP1/Group10.java
@@ -13,7 +13,26 @@
  */
 
 public class Group10 {
-  public static void main(String[] args) {
+  public static final int INT_RANGE_MIN = 100;
+  public static final int INT_RANGE_MAX = 999;
 
+  public static void main(String[] args) {
+    int intSum = 0;
+
+    for (int intI = INT_RANGE_MIN; intI <= INT_RANGE_MAX; intI++) {
+      int intDigit1 = intI / 100;
+      int intDigit2 = (intI / 10) % 10;
+      int intDigit3 = intI % 10;
+
+      int intCube1 = intDigit1 * intDigit1 * intDigit1;
+      int intCube2 = intDigit2 * intDigit2 * intDigit2;
+      int intCube3 = intDigit3 * intDigit3 * intDigit3;
+
+      if (intI == intCube1 + intCube2 + intCube3) {
+        intSum += intI;
+        System.out.print  (intI + " ");
+      }
+    }
+    System.out.println("\nSum: " + intSum);
   }
 }
