02eb9a6977fa44d46bbb2d1082ddbe59eee5a492
Jamaica A. Castillo <castillojamaica00@gmail.com>
2025-09-21 17:53:30 +0800

    ...

--- MP1/Group05.java
+++ MP1/Group05.java
@@ -14,6 +14,42 @@
 
 public class Group05 {
   public static void main(String[] args) {
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++){
+        System.out.print ("* ");
+      }
+      System.out.println();
+    }
 
+    System.out.println ();
+
+    for (int i = n; i >= 1; i--) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print ("* ");
+      }
+      System.out.println();
+    }
+
+    System.out.println ();
+
+    for (int i = 1; i <= n; i++){
+      for (int j = 1; j <= i; j++){
+        System.out.print (j + " ");
+      }
+      System.out.println();
+    }
+
+    System.out.println();
+
+    for (int i = 1; i <= n; i++){
+      for (int k = n - 1; k >= i; k--){
+        System.out.print (" ");
+      }
+      for (int j = 1; j <= i; j++){
+        System.out.print ("* ");
+      }
+      System.out.println();
+    }
   }
 }
