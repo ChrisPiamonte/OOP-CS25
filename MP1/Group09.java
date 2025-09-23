d62a2c60abd5037c7d72c06976e8c2c4b6986610
Lnzarn <enriquelanzaron@gmail.com>
2025-09-23 16:17:05 +0800

    Uploading the first draft of the program

--- MP1/Group09.java
+++ MP1/Group09.java
@@ -1,19 +1,28 @@
-/*
- * ==========================================================
- *  Machine Problem 1 - Workspace for Group 09
- * ==========================================================
- *
- * Instructions:
- * - This file is your workspace for solving Machine Problem 1.
- * - You will practice the Programming Core Concepts discussed in class,
- *   such as variables, data types, operators, and basic string operations.
- * - Do NOT create additional classes or advanced features (like arrays)
- *   unless instructed.
- * ==========================================================
- */
-
 public class Group09 {
   public static void main(String[] args) {
+    int intInputNumber = 12321;
+    System.out.println("Number " + intInputNumber +
+      (checkPalindrome(intInputNumber) ? " is" : " is not") + " a Palindrome");
 
+    System.out.println("Palindromic numbers from 1-500:");
+    for (int intLoopNumber = 1; intLoopNumber <= 500; intLoopNumber++) {
+      if (checkPalindrome(intLoopNumber)) {
+        System.out.print(intLoopNumber + " ");
+      }
+    }
+  }
+
+  public static boolean checkPalindrome(int intNumber) {
+    return intNumber == reverseNumber(intNumber);
+  }
+
+  public static int reverseNumber(int intNumber) {
+    String strReversedNumber = "";
+    while (intNumber > 0) {
+      int intLastDigit = intNumber % 10;
+      strReversedNumber += intLastDigit;
+      intNumber /= 10;
+    }
+    return Integer.parseInt(strReversedNumber);
   }
 }
