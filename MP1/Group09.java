47ea9ecabd2d73a08beff0ec5b4d15c1091a45cc
Lnzarn <enriquelanzaron@gmail.com>
2025-09-23 22:25:25 +0800

    Removed the use of string & another alternative way without string and multiplication

--- MP1/Group09.java
+++ MP1/Group09.java
@@ -1,25 +1,28 @@
-public class Group09 {
-  public static void main(String[] args) {
-    int intInputNumber = 12321;
-    System.out.println("Number " + intInputNumber +
-      (checkPalindrome(intInputNumber) ? " is" : " is not") + " a Palindrome");
+public class Group09{
+    public static void main(String[] args) {
+        int intInputNumber = 12321;
+        System.out.println("Number " + intInputNumber +
+                (checkPalindrome(intInputNumber) ? " is" : " is not") + " a Palindrome");
 
-    System.out.println("Palindromic numbers from 1-500:");
-    for (int intLoopNumber = 1; intLoopNumber <= 500; intLoopNumber++) {
-      if (checkPalindrome(intLoopNumber)) {
-        System.out.print(intLoopNumber + " ");
-      }
+        System.out.println("Palindromic numbers from 1-500:");
+        for (int intLoopNumber = 1; intLoopNumber <= 500; intLoopNumber++) {
+            if (checkPalindrome(intLoopNumber)) {
+                System.out.print(intLoopNumber + " ");
+            }
+        }
     }
-  }
-
-  public static boolean checkPalindrome(int intNumber) {
-    int intOriginalNumber = intNumber;
-    String strReversedNumber = "";
-    while (intNumber > 0) {
-      int intLastDigit = intNumber % 10;
-      strReversedNumber += intLastDigit;
-      intNumber /= 10;
+    public static boolean checkPalindrome(int intNum) {
+        int intOriginal = intNum;
+        int intReverse = 0;
+        while (intNum != 0) {
+            int intDigit = intNum % 10;
+            int intSum = 0;
+            for (int i = 0; i < 10; i++) {
+                intSum += intReverse;
+            }
+            intReverse = intSum + intDigit;
+            intNum = intNum / 10;
+        }
+        return intOriginal == intReverse;
     }
-    return intOriginalNumber  == Integer.parseInt(strReversedNumber);
-  }
-} 
+}
