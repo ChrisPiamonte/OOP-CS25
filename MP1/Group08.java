88ab3c2450e23029f0d3c2db19d73d601a35c723
Rei <reiyukawa7@gmail.com>
2025-09-24 17:20:21 +0800

    .

--- MP1/Group08.java
+++ MP1/Group08.java
@@ -1,43 +1,19 @@
+/*
+ * ==========================================================
+ *  Machine Problem 1 - Workspace for Group 08
+ * ==========================================================
+ *
+ * Instructions:
+ * - This file is your workspace for solving Machine Problem 1.
+ * - You will practice the Programming Core Concepts discussed in class,
+ *   such as variables, data types, operators, and basic string operations.
+ * - Do NOT create additional classes or advanced features (like arrays)
+ *   unless instructed.
+ * ==========================================================
+ */
+
 public class Group08 {
   public static void main(String[] args) {
-        int start = 1;
-        int end = 200;
-        int sumEven = 0;
-        int sumOdd = 0;
-        int evenDiv5 = 0;
-        int oddsDiv5 = 0;
-        int div3 = 0;
 
-        for(int i = start; i <= end; i++) {
-            if(i % 2 == 0){
-                sumEven += i; 
-                if(i % 5 == 0) {
-                    evenDiv5++;
-                }
-            }
-            if(i % 2 == 1){
-                sumOdd += i;
-                if(i % 5 == 0) {
-                    oddsDiv5++;
-                }
-            }
-            if(i % 3 == 0){
-                div3++;
-            }
-        }
-        System.out.println("Sum of even numbers: " + sumEven);
-        System.out.println("Sum of odd numbers: " + sumOdd);
-        System.out.println("Difference of the sum of even and odd numbers: " + (sumEven - sumOdd));
-        
-        if(evenDiv5 > oddsDiv5) {
-            System.out.println("Even has more number divisible by 5");
-        } else if(evenDiv5 < oddsDiv5) {
-            System.out.println("Odd has more number divisible by 5");
-        } else {
-            System.out.println("Both odd and even have the same number of numbers divisible by 5");
-        } 
-
-        double percentage = (div3 * 100.0) / (end - start + 1); 
-        System.out.printf("The percentage of the numbers divisible by 3 in the range is %.2f%%%n", percentage);
   }
 }
