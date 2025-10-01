370fe028ea8ef9afda4442b86090e7d82460e8f0
Rachel Anne Cilon <annerachel095@gmail.com>
2025-10-01 15:48:33 +0800

    inital commit

---
+++ MP2/MP2-G3/Item3.java
@@ -0,0 +1,13 @@
+/*
+ * 3. Strong Number
+ * Write a program that checks if a given number is a 
+ * Strong Number. A Strong Number is one where the sum 
+ * of the factorial of its digits equals the number itself 
+ * (e.g., 145 because 1! + 4! + 5! = 145).
+ */
+
+public class Item3{
+    public static void main(String[] args) {
+        
+    }
+}
