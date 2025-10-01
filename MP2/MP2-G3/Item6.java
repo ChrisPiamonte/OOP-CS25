56047cf2f9af9cdb25da244800f13542728d8a04
Rachel Anne Cilon <annerachel095@gmail.com>
2025-10-01 15:55:58 +0800

    initial commit

---
+++ MP2/MP2-G3/Item6.java
@@ -0,0 +1,12 @@
+/*
+ * 6. Prime Factors of a Number 
+ * Ask the user to enter a number and display 
+ * all its prime factors. For example, the prime 
+ * factors of 84 are 2, 2, 3, 7.
+ */
+
+public class Item6 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ MP2/MP2-G3/Item8.java
@@ -0,0 +1,10 @@
+/*
+ * 8. Automorphic Number 
+ * Create a program to check if a number is 
+ * an Automorphic Number. A number is Automorphic if its
+ * square ends with the number itself (e.g., 762 = 5776).
+ */
+
+public class Item8 {
+    
+}
