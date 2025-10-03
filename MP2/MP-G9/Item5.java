8339005942a4e254596ed9fecc1dd3188c47960c
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-10-04 01:39:33 +0800

    minor fixes

--- MP2/MP-G9/Item5.java
+++ MP2/MP-G9/Item5.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+// Kaprekar Number
 
 public class Item5 {
 
-    static boolean boolKaprekar(int intNumber) {
+    static boolean boolKaprekar (int intNumber) {
         if (intNumber == 1) {
             return true;
         }
@@ -16,20 +17,8 @@ public class Item5 {
             String strLeft = strSquare.substring(0, i);
             String strRight = strSquare.substring(i);
 
-            int intLeft;
-            int intRight;
-
-            if (strLeft.isEmpty()) {
-                intLeft = 0;
-            } else {
-                intLeft = Integer.parseInt(strLeft);
-            }
-
-            if (strRight.isEmpty()) {
-                intRight = 0;
-            } else {
-                intRight = Integer.parseInt(strRight);
-            }
+            int intLeft = Integer.parseInt(strLeft);
+            int intRight = Integer.parseInt(strRight);
 
             if (intLeft + intRight == intNumber && intRight != 0) {
                 return true;
@@ -45,9 +34,9 @@ public class Item5 {
         int intNumber = sc.nextInt();
 
         if (boolKaprekar(intNumber)) {
-            System.out.println(intNumber + " is a Kaprekar Number.");
+            System.out.println("["+intNumber+"] IS a Kaprekar Number.");
         } else {
-            System.out.println(intNumber + " is NOT a Kaprekar Number.");
+            System.out.println("["+intNumber+"] is NOT a Kaprekar Number.");
         }
 
         sc.close();
