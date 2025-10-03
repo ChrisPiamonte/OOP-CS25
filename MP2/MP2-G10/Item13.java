4c5c82d10f71b95dcbdf464cc4d986f9f9826644
Izeah Arquillano <izeaharquillano@gmail.com>
2025-10-03 23:04:51 +0800

    suffering

--- MP2/MP2-G10/Item13.java
+++ MP2/MP2-G10/Item13.java
@@ -10,14 +10,14 @@ public class Item13 {
     int intArrN = 0;
 
     while (!boolIsValidMatrix) {
-      System.out.print("Enter matrix size (m x n): ");
-      strArrMxSize = buffread.readLine().split(" x ");
+      System.out.print("Enter matrix 1 size (m x n): ");
+      strArrMxSize = buffread.readLine().trim().split(" x ");
       if (strArrMxSize.length != 2) {
         continue;
       }
       intArrM = Integer.parseInt(strArrMxSize[0]);
       intArrN = Integer.parseInt(strArrMxSize[1]);
-      break;
+      boolIsValidMatrix = true;
     }
 
     System.out.printf("%dx%d matrix\n", intArrM, intArrN);
@@ -28,16 +28,62 @@ public class Item13 {
     int intArrM1 = 0;
 
     while (!boolIsValidMatrix) {
-      System.out.print("Enter matrix size (m x n): ");
-      strArrMxSize1 = buffread.readLine().split(" x ");
-      if (strArrMxSize1.length != 2) {
-        continue;
-      }
+      System.out.print("Enter matrix 2 size (m x n): ");
+      strArrMxSize1 = buffread.readLine().trim().split(" x ");
+      if (strArrMxSize1.length != 2) continue;
       intArrM1 = Integer.parseInt(strArrMxSize1[0]);
       intArrN1 = Integer.parseInt(strArrMxSize1[1]);
-      break;
+      if (intArrN != intArrM1) continue;
+      boolIsValidMatrix = true;
     }
 
     System.out.printf("%dx%d matrix\n", intArrM1, intArrN1);
+
+    int[][] intMx = new int[intArrM][intArrN];
+    System.out.println("Enter Elements for matrix 1:");
+
+    for (int intI = 0; intI < intArrM; intI++) {
+      boolean boolIsValidInput = false;
+      while (!boolIsValidInput) {
+        System.out.printf("Row %d: ", intI + 1);
+        String[] strArrTemp = buffread.readLine().trim().split(" ");
+        if (strArrTemp.length != intArrN) continue;
+        for (int intJ = 0; intJ < intArrN; intJ++) {
+          intMx[intI][intJ] = Integer.parseInt(strArrTemp[intJ]);
+        }
+        boolIsValidInput = true;
+      }
+    }
+
+    for (int intI = 0; intI < intArrM; intI++) {
+      for (int intJ = 0; intJ < intArrN; intJ++) {
+        System.out.print(intMx[intI][intJ] + " ");
+      }
+      System.out.println();
+    }
+
+    int[][] intMx1 = new int[intArrM1][intArrN1];
+    System.out.println("Enter Elements for matrix 2:");
+
+    for (int intI = 0; intI < intArrM1; intI++) {
+      boolean boolIsValidInput = false;
+      while (!boolIsValidInput) {
+        System.out.printf("Row %d: ", intI + 1);
+        String[] strArrTemp = buffread.readLine().trim().split(" ");
+        if (strArrTemp.length != intArrN1) continue;
+        for (int intJ = 0; intJ < intArrN1; intJ++) {
+          intMx1[intI][intJ] = Integer.parseInt(strArrTemp[intJ]);
+        }
+        boolIsValidInput = true;
+      }
+    }
+
+    for (int intI = 0; intI < intArrM1; intI++) {
+      for (int intJ = 0; intJ < intArrN1; intJ++) {
+        System.out.print(intMx1[intI][intJ] + " ");
+      }
+      System.out.println();
+    }
+
   }
 }
