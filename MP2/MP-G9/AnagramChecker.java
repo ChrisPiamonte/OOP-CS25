f49b8e075dd116d60c9fab41c4dc852cb4a2e680
Pazzzyyy <laurencehernandezpaz@gmail.com>
2025-10-04 07:11:13 +0800

    Update AnagramChecker.java
    Double Checking

--- MP2/MP-G9/AnagramChecker.java
+++ MP2/MP-G9/AnagramChecker.java
@@ -3,18 +3,15 @@ import java.util.Scanner;
 
 public class AnagramChecker {
     public static boolean areAnagrams(String str1, String str2) {
-        // Remove spaces and convert to lowercase
         str1 = str1.replaceAll("\\s", "").toLowerCase();
         str2 = str2.replaceAll("\\s", "").toLowerCase();
 
-        // Convert strings to char arrays and sort
         char[] arr1 = str1.toCharArray();
         char[] arr2 = str2.toCharArray();
 
         Arrays.sort(arr1);
         Arrays.sort(arr2);
 
-        // Check if sorted arrays are equal
         return Arrays.equals(arr1, arr2);
     }
 
