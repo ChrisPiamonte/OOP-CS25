643c2dc2280c2de7c951b7b50691fec0c2413b92
Trisha Mae Mercado <trishamaemercado06@gmail.com>
2025-10-05 20:16:56 +0800

    class corrected

--- MP2/MP-G2/item1.java
+++ MP2/MP-G2/item1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Item1{
+public class item1{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in); //Scanner object
 
