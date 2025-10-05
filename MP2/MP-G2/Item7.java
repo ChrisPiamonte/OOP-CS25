9b2699cfc1c613e7c975992fe001f443443303f0
Joshua De Mesa <deathilim2@gmail.com>
2025-10-05 18:31:15 +0800

    Changed to long, and added negative case

--- MP2/MP-G2/Item7.java
+++ MP2/MP-G2/Item7.java
@@ -7,38 +7,47 @@ import java.util.Scanner;
  * Ex: (11, 13), (17, 19)...
  * */
 
-public class Item7 {
+public class Item7{
     public static void main(String[] args) {
         // Initialize the scanner
         Scanner scan = new Scanner(System.in);
 
         // Standard variables
+        long longRange = 0;
         System.out.print("Enter a number: ");
-        while(!scan.hasNextInt()) {
-            System.out.print("Please enter a valid number: ");
-            scan.nextLine();
+        while (true) {
+            while (!scan.hasNextLong()) { // Non-numbers input
+                System.out.print("Please enter a valid number: ");
+                scan.nextLine();
+            }
+            longRange = scan.nextLong();
+            if (longRange < 0) { // Negative numbers
+                System.out.print("Please enter a valid number: ");
+                scan.nextLine();
+                continue;
+            }
+            break; 
         }
-        int intRange = scan.nextInt();
 
-        twinPrimes(intRange); // Calls method to print all twin primes to n range
+        twinPrimes(longRange); // Calls method to print all twin primes to n range
 
         scan.close();
     }
 
-    public static void twinPrimes(int range) {
+    public static void twinPrimes(long range) {
         // Add the skipped part in the loop
         if (range >= 5) {
             System.out.print("(3, 5) ");
         }
     
-        for (int i = 5; i <= range; i+=6) { // Start from 5 onwards, and increment 6 (More than that, it will skip parts)
+        for (long i = 5; i <= range; i+=6) { // Start from 5 onwards, and increment 6 (More than that, it will skip parts)
             if (isPrime(i) && isPrime(i + 2)) {
                 System.out.printf("(%d, %d) ", i, i + 2); // Outputs Twin Primes
             }
         }
     }
 
-    public static boolean isPrime(int num) {
+    public static boolean isPrime(long num) {
         // Standard prime checks
         if (num <= 1) 
             return false;
@@ -48,7 +57,7 @@ public class Item7 {
             return false;
 
         // Checks primes beyond 25    
-        for (int i = 5; i * i <= num; i+=6) {
+        for (long i = 5; i * i <= num; i+=6) {
             if (num % i == 0 || num % (i + 2) == 0) {
                 return false;
             }
