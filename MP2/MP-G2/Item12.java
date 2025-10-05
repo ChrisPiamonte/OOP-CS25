2df7732cf9f0c7a2dfdad32100c8ff58a756351b
Gabriel Benedict Brillo <appzord@gmail.com>
2025-10-05 22:46:45 +0800

    Update Item12.java to fix class mismatch

--- MP2/MP-G2/Item12.java
+++ MP2/MP-G2/Item12.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class WordCount {
+public class Item12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String strSentence;
