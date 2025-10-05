3e4570cae738964bc66edd97927d11a25a088d86
Rachel Anne Cilon <annerachel095@gmail.com>
2025-10-05 22:05:29 +0800

    Update, added user input and file handling funct

--- MP2/MP2-G3/Item3.java
+++ MP2/MP2-G3/Item3.java
@@ -6,6 +6,9 @@
  * (e.g., 145 because 1! + 4! + 5! = 145).
  */
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Item3{
     static boolean isStrongNumber(int number){
         int temp=number;
@@ -30,13 +33,33 @@ public class Item3{
     }
 
     public static void main(String[] args) {  
-        boolean example_1 = isStrongNumber(145); // true
-        boolean example_2= isStrongNumber(25); // false
-        boolean example_3 = isStrongNumber(1); // true
+        // Using Scanner
+        Scanner objScanner = new Scanner(System.in);
 
-        System.out.println("Is strong number?");
-        System.out.println("145? " + example_1);
-        System.out.println("25? " + example_2);
-        System.out.println("1? " + example_3);
+        System.err.println("Strong Number Checker");
+        int number = 0;
+        boolean validInput= false;
+
+        //get input from user - loop until input is valid
+        while(!validInput){
+            try {
+                System.out.print("Enter a number: ");
+                number = objScanner.nextInt();
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Error. Please enter a valid integer.");
+                objScanner.nextLine();
+            }
+        }
+
+        boolean example = isStrongNumber(number);
+
+        if(example){
+            System.out.println("This number is a Strong Number!");
+        } else {
+            System.out.println("This number is not a Strong Number.");
+        }
+
+        objScanner.close();
     }
 }
