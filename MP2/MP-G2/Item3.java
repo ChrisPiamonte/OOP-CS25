666e7634a202e90b5341109972c7b4fd31f3d675
Joshua De Mesa <deathilim2@gmail.com>
2025-10-05 18:02:16 +0800

    Changed int to long, added error case for negative numbers

--- MP2/MP-G2/Item3.java
+++ MP2/MP-G2/Item3.java
@@ -9,52 +9,61 @@ import java.util.Scanner;
  * Therefore, 145 is a Strong Number.
  */
 
-public class Item3{
+public class StrongNumber {
     public static void main(String[] args) {
         // Initialize scanners
         Scanner scan = new Scanner(System.in);
 
         // Standard variables
-        int intUserInput;
-        int intLastDigit;
-        int intSumOfDigits = 0;
+        long longUserInput = 0;
+        long longLastDigit;
+        long longSumOfDigits = 0;
        
         // Get last digit, factorial each.
         System.out.print("Enter a number: ");
 
-        while (!scan.hasNextInt()) {
-            System.out.print("Please enter a valid number: ");
-            scan.nextLine();
+        // Repeats until user enters a valid number
+        while (true) {
+            while (!scan.hasNextLong()) { // Non-numbers input
+                System.out.print("Please enter a valid number: ");
+                scan.nextLine();
+            }
+            longUserInput = scan.nextLong();
+            if (longUserInput < 0) { // Negative numbers
+                System.out.print("Please enter a valid number: ");
+                scan.nextLine();
+                continue;
+            }
+            break; 
         }
-        intUserInput = scan.nextInt();
 
         // A variable containing the original value
-        int intOriginalValue = intUserInput;
+        long longOriginalValue = longUserInput;
 
         // Gets the factorial of each digit
-        while (intUserInput != 0) {
-            intLastDigit = intUserInput % 10; // Gets the last digit
-            intSumOfDigits += factorial(intLastDigit); // Gets the factorial
+        while (longUserInput != 0) {
+            longLastDigit = longUserInput % 10; // Gets the last digit
+            longSumOfDigits += factorial(longLastDigit); // Gets the factorial
      
-            System.out.printf("%d! = %d\n", intLastDigit, factorial(intLastDigit)); // Print output
+            System.out.printf("%d! = %d\n", longLastDigit, factorial(longLastDigit)); // Print output
 
             // If the sum is already bigger than the input, exit.
-            if (intSumOfDigits > intOriginalValue) {
-                System.out.printf("%d is not a sStrong Number\n", intOriginalValue);
+            if (longSumOfDigits > longOriginalValue) {
+                System.out.printf("%d is not a Strong Number\n", longOriginalValue);
                 System.exit(0);
             } 
 
-            intUserInput /= 10; // Truncate the last digit
+            longUserInput /= 10; // Truncate the last digit
         }
         
         // Show the sum
-        System.out.printf("The sum of the factorial of each digit is %d\n", intSumOfDigits);
+        System.out.printf("The sum of the factorial of each digit is %d\n", longSumOfDigits);
 
         // Checks if Strong Number
-        if (intSumOfDigits == intOriginalValue) {
-            System.out.printf("%d is a Strong Number\n", intSumOfDigits);        
+        if (longSumOfDigits == longOriginalValue) {
+            System.out.printf("%d is a Strong Number\n", longSumOfDigits);        
         } else {
-            System.out.printf("%d is not a Strong Number\n", intOriginalValue);
+            System.out.printf("%d is not a Strong Number\n", longOriginalValue);
         }
 
         scan.close();
@@ -63,7 +72,7 @@ public class Item3{
     }
 
     // Factorial
-    public static int factorial(int n) {
+    public static long factorial(long n) {
         if (n == 0) {
             return 1;
         } else {
