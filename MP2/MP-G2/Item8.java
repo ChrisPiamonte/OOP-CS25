8a0584eb6a702df126aae7266b6c7af75f9652d7
Trisha Mae Mercado <trishamaemercado06@gmail.com>
2025-10-05 17:32:40 +0800

    Update Item8.java

--- MP2/MP-G2/Item8.java
+++ MP2/MP-G2/Item8.java
@@ -1,12 +1,27 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Item8{
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in); //Scanner object as always
-
-        System.out.print("Please enter a number: ");
-        int intNumber = scanner.nextInt(); //Read the integer input.
-
+        int intNumber = 0;
+        boolean correctInput = false; //The correct input is false first
+        
+        while(!correctInput){
+            System.out.print("Please enter a positive integer: ");
+            try {
+                intNumber = scanner.nextInt();
+                if(intNumber <= 0){
+                    System.out.println("Please enter a positive integer again.");
+                } else {
+                    correctInput = true;//Input must be correct here
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a positive number.");
+                scanner.next();//Prevents infinite loop.
+            }
+        }
+        
         if(isAutomorphic(intNumber)){
             System.out.println("This number is automorphic.");
         } else {
@@ -15,7 +30,7 @@ public class Item8{
          
     scanner.close();
     }
-
+    //Boolean function to check if it is automorphic or not
     public static boolean isAutomorphic(int intNum){
         long square = (long) intNum * intNum; //Finds the square of the number.
         //Converts both the number and its square to compare 
