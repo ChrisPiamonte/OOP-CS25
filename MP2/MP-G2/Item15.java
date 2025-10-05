60eca2e095d02582dddd86bb82d63ec48e40e10b
Gabriel Benedict Brillo <appzord@gmail.com>
2025-10-05 22:47:19 +0800

    Update Item15.java to fix class mismatch

--- MP2/MP-G2/Item15.java
+++ MP2/MP-G2/Item15.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Problem15_ArrayFrequency {
+public class Item15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("FREQUENCY NUMBER GENERATOR");
