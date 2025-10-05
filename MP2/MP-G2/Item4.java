c7fa8525155728c68480b7a8c698cd6e2b05974b
Gabriel Benedict Brillo <appzord@gmail.com>
2025-10-05 22:46:24 +0800

    Update Item4.java to fix class mismatch

--- MP2/MP-G2/Item4.java
+++ MP2/MP-G2/Item4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HappyNumber {
+public class Item4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("HAPPY NUMBER CHECKER");
