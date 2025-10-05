40011eed0e673da1277b21443df5fbec57d0601c
Gabriel Benedict Brillo <appzord@gmail.com>
2025-10-05 20:29:30 +0800

    Create Item15.java - Frequency Number Generator

---
+++ MP2/MP-G2/Item15.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Problem15_ArrayFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("FREQUENCY NUMBER GENERATOR");
+        System.out.print("Enter number of elements: ");
+        while (!sc.hasNextInt()) { 
+                System.out.print("\tInvalid.\n\tPlease enter a valid choice: ");
+                sc.next(); 
+            }
+        int intNum = sc.nextInt();
+        int[] intArr = new int[intNum];
+        boolean[] blnVisited = new boolean[intNum];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < intNum; i++){ 
+        while (!sc.hasNextInt()) { 
+                System.out.print("\tInvalid.\n\tPlease enter a valid choice: ");
+                sc.next(); 
+            }
+        intArr[i] = sc.nextInt();
+        }
+        
+        for (int i = 0; i < intNum; i++) {
+            if (blnVisited[i]) 
+            continue;
+            
+            int intCount = 1;
+            for (int j = i + 1; j < intNum; j++) {
+                if (intArr[i] == intArr[j]) {
+                    blnVisited[j] = true;
+                    intCount++;
+                }
+            }
+            System.out.println(intArr[i] + " appears " + intCount + " times");
+        }
+        System.out.println("Program Ended Succesfully.");
+    }
+}
