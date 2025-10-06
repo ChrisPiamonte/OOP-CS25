1a6a3d08d209d014463b6f9ab852ad0d203c55a0
Trisha Mae Mercado <trishamaemercado06@gmail.com>
2025-10-06 11:37:23 +0800

    Changed while loop statement in Item1.java

--- MP2/MP-G1/Item1.java
+++ MP2/MP-G1/Item1.java
@@ -5,32 +5,28 @@ public class Item1{
         Scanner scanner = new Scanner(System.in); //Scanner object
         int intNum = 0;
 
-        while(true){//Infinite loop for invalid inputs until the input is true
-            System.out.print("Enter a positive integer only: ");
-
-             if (scanner.hasNextInt()){
-                intNum = scanner.nextInt();
-
-                if(intNum > 0){
-                    break; //Exit infinite loops
-                } else {
-                    System.out.print("Enter a positive integer only: ");//Ask it again
-                }
-            } else {
-                System.out.println("Input invalid. Enter numbers only");
-                scanner.next(); //discard invalid inputs from the user
+        while (true) {
+            System.out.print("Enter a number: ");
+            if (!scanner.hasNextInt()) { 
+                System.out.print("\tInvalid.\n\tPlease enter a valid integer.\n");
+                scanner.next(); 
+                continue;
             }
+            intNum = scanner.nextInt();
+            
+            if (intNum <= 0) {
+                System.out.print("\tInvalid.\n\tPlease enter a positive integer only.\n");
+                continue;
+            }
+            break; 
         }
-            
-        int sumDivisors = 1; //Initialize it as 1.
 
-        for(int i = 2;  i <= intNum / 2; i++){ //For loop from 2 up to half of the number
-            if(intNum % i == 0){ //i is the divisor of the numbers
-                sumDivisors += i; //add i to the sum of divisors
-            }
-        }//compare the proper divisors to the input number
-            
-            if(sumDivisors == intNum){
+        int intSum = 0;
+        for (int i = 1; i <= intNum / 2; i++) {
+            if (intNum % i == 0) 
+                intSum += i;
+        }  
+            if(intSum == intNum){
                 System.out.println(intNum + " is a perfect number.");
             } else {
                 System.out.println(intNum + " is not a perfect number.");
