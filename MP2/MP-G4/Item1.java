472b9de009be3b73124c2e19e5f8b01e3c1085ec
Kent <khieonqt@gmail.com>
2025-10-06 12:38:34 +0800

    Update variable name

--- MP2/MP-G4/Item1.java
+++ MP2/MP-G4/Item1.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Item1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n;
+        int number;
 
         // Ask user for a positive number input
         while (true) { 
             System.out.print("Enter a number: ");
-            n = scan.nextInt();
+            number = scan.nextInt();
 
-            if (n > 0) {
+            if (number > 0) {
                 break;
             }
 
@@ -18,16 +18,16 @@ public class Item1 {
         }
 
         int sum = 0;
-        for (int i = n / 2; i > 0; i--) {
-            if (n % i == 0) {
+        for (int i = number / 2; i > 0; i--) {
+            if (number % i == 0) {
                 sum += i;
             }
         }
 
-        if (sum == n) {
-            System.out.println(n + " is a Perfect Number.");
+        if (sum == number) {
+            System.out.println(number + " is a Perfect Number.");
         } else {
-            System.out.println(n + " is not a Perfect Number.");
+            System.out.println(number + " is not a Perfect Number.");
         }
             
         scan.close();
